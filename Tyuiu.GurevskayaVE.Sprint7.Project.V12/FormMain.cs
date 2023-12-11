139fe8f4996dcdc990785af4ead08e24c0879649
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-12-11 21:54:32 +0500

    q

--- Tyuiu.GurevskayaVE.Sprint7.Project.V12/FormMain.cs
+++ Tyuiu.GurevskayaVE.Sprint7.Project.V12/FormMain.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tyuiu.GurevskayaVE.Sprint7.Project.V12.Lib;
 
 namespace Tyuiu.GurevskayaVE.Sprint7.Project.V12
 {
@@ -16,7 +17,7 @@ namespace Tyuiu.GurevskayaVE.Sprint7.Project.V12
         {
             InitializeComponent();
         }
-
+        DataService ds = new DataService();
         private void buttonInfo_GVE_Click(object sender, EventArgs e)
         {
             FormMain formabout = new FormMain();
