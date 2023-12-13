4f5b7268bc240243cb0f6989ea990db3c13848e6
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-12-13 12:46:38 +0500

    hi

--- Tyuiu.GurevskayaVE.Sprint7.Project.V12/FormStat.Designer.cs
+++ Tyuiu.GurevskayaVE.Sprint7.Project.V12/FormStat.Designer.cs
@@ -29,14 +29,21 @@ namespace Tyuiu.GurevskayaVE.Sprint7.Project.V12
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panelLeft_GVE = new System.Windows.Forms.Panel();
             this.buttonSredOZU_GVE = new System.Windows.Forms.Button();
             this.textBoxSredOZU_GVE = new System.Windows.Forms.TextBox();
+            this.chartDiag_GVE = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonDiag_GVE = new System.Windows.Forms.Button();
             this.panelLeft_GVE.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartDiag_GVE)).BeginInit();
             this.SuspendLayout();
             // 
             // panelLeft_GVE
             // 
+            this.panelLeft_GVE.Controls.Add(this.buttonDiag_GVE);
             this.panelLeft_GVE.Controls.Add(this.buttonSredOZU_GVE);
             this.panelLeft_GVE.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelLeft_GVE.Location = new System.Drawing.Point(0, 0);
@@ -46,13 +53,15 @@ namespace Tyuiu.GurevskayaVE.Sprint7.Project.V12
             // 
             // buttonSredOZU_GVE
             // 
+            this.buttonSredOZU_GVE.BackColor = System.Drawing.Color.MediumAquamarine;
             this.buttonSredOZU_GVE.Font = new System.Drawing.Font("Corbel", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSredOZU_GVE.ForeColor = System.Drawing.SystemColors.ControlText;
             this.buttonSredOZU_GVE.Location = new System.Drawing.Point(12, 12);
             this.buttonSredOZU_GVE.Name = "buttonSredOZU_GVE";
             this.buttonSredOZU_GVE.Size = new System.Drawing.Size(105, 94);
             this.buttonSredOZU_GVE.TabIndex = 0;
             this.buttonSredOZU_GVE.Text = "Среднее значение объема памяти";
-            this.buttonSredOZU_GVE.UseVisualStyleBackColor = true;
+            this.buttonSredOZU_GVE.UseVisualStyleBackColor = false;
             this.buttonSredOZU_GVE.Click += new System.EventHandler(this.buttonSredOZU_GVE_Click);
             // 
             // textBoxSredOZU_GVE
@@ -63,17 +72,46 @@ namespace Tyuiu.GurevskayaVE.Sprint7.Project.V12
             this.textBoxSredOZU_GVE.Size = new System.Drawing.Size(100, 22);
             this.textBoxSredOZU_GVE.TabIndex = 1;
             // 
+            // chartDiag_GVE
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartDiag_GVE.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartDiag_GVE.Legends.Add(legend1);
+            this.chartDiag_GVE.Location = new System.Drawing.Point(149, 129);
+            this.chartDiag_GVE.Name = "chartDiag_GVE";
+            series1.ChartArea = "ChartArea1";
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartDiag_GVE.Series.Add(series1);
+            this.chartDiag_GVE.Size = new System.Drawing.Size(410, 289);
+            this.chartDiag_GVE.TabIndex = 2;
+            // 
+            // buttonDiag_GVE
+            // 
+            this.buttonDiag_GVE.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.buttonDiag_GVE.Location = new System.Drawing.Point(13, 184);
+            this.buttonDiag_GVE.Name = "buttonDiag_GVE";
+            this.buttonDiag_GVE.Size = new System.Drawing.Size(105, 94);
+            this.buttonDiag_GVE.TabIndex = 1;
+            this.buttonDiag_GVE.Text = "Диагонали ЭВМ";
+            this.buttonDiag_GVE.UseVisualStyleBackColor = false;
+            this.buttonDiag_GVE.Click += new System.EventHandler(this.buttonDiag_GVE_Click);
+            // 
             // FormStat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(659, 603);
+            this.Controls.Add(this.chartDiag_GVE);
             this.Controls.Add(this.textBoxSredOZU_GVE);
             this.Controls.Add(this.panelLeft_GVE);
             this.Name = "FormStat";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Статистические данные";
             this.panelLeft_GVE.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartDiag_GVE)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,5 +122,7 @@ namespace Tyuiu.GurevskayaVE.Sprint7.Project.V12
         private System.Windows.Forms.Panel panelLeft_GVE;
         private System.Windows.Forms.Button buttonSredOZU_GVE;
         private System.Windows.Forms.TextBox textBoxSredOZU_GVE;
+        private System.Windows.Forms.Button buttonDiag_GVE;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartDiag_GVE;
     }
 }
--- Tyuiu.GurevskayaVE.Sprint7.Project.V12/FormStat.cs
+++ Tyuiu.GurevskayaVE.Sprint7.Project.V12/FormStat.cs
@@ -22,5 +22,19 @@ namespace Tyuiu.GurevskayaVE.Sprint7.Project.V12
 
             textBoxSredOZU_GVE.Text = "11,3 гб";
         }
+
+        private void buttonDiag_GVE_Click(object sender, EventArgs e)
+        {
+            this.chartDiag_GVE.Series["Series1"].Points.AddXY("Apple MacBook Air 13 M1", 13.3);
+            this.chartDiag_GVE.Series["Series1"].Points.AddXY("Lenovo IdeaPad Gaming 3", 16);
+            this.chartDiag_GVE.Series["Series1"].Points.AddXY("Thunderobot 911 Air X", 15.6);
+            this.chartDiag_GVE.Series["Series1"].Points.AddXY("ASUS M1502IA", 15.6);
+            this.chartDiag_GVE.Series["Series1"].Points.AddXY("HONOR MagicBook X 16", 16);
+            this.chartDiag_GVE.Series["Series1"].Points.AddXY("Irbis NB265", 14.1);
+            this.chartDiag_GVE.Series["Series1"].Points.AddXY("Apple MacBook Pro 14", 14.2);
+            this.chartDiag_GVE.Series["Series1"].Points.AddXY("HUAWEI Matebook D 15 i5", 15.6);
+            this.chartDiag_GVE.Series["Series1"].Points.AddXY("MSI Katana GF76", 17.3);
+
+        }
     }
 }
