7d111a9a064ae1af1e8f8397025993fdc934f8a6
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-12-19 19:21:00 +0500

    genius GENIUS 0.0 ^-^

--- Tyuiu.GurevskayaVE.Sprint7.Project.V12.test/DataServiceTest.cs
+++ Tyuiu.GurevskayaVE.Sprint7.Project.V12.test/DataServiceTest.cs
@@ -19,6 +19,44 @@ namespace Tyuiu.GurevskayaVE.Sprint7.Project.V12.test
             Assert.AreEqual(wait, fileex);
 
         }
-        
+
+        string filePath = @"C:\Users\ВАЛЕРИЯ 2004\source\repos\Tyuiu.GurevskayaVE.Sprint7\Tyuiu.GurevskayaVE.Sprint7.Project.V12\bin\Debug\InPutEC_Sprint7.csv";
+
+        public static string[,] LoadFromFileData(string filePath)
+        {
+            string fileData = File.ReadAllText(filePath);
+            fileData = fileData.Replace('\n', '\r');
+            string[] lines = fileData.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            int rows = lines.Length;
+            int columns = lines[0].Split(';').Length;
+            string[,] arrayValues = new string[rows, columns];
+
+            for (int r = 0; r < rows; r++)
+            {
+                string[] line_r = lines[r].Split(';');
+                for (int c = 0; c < columns; c++)
+                {
+                    arrayValues[r, c] = line_r[c];
+                }
+            }
+
+            return arrayValues;
+        }
+
+        [TestMethod]
+        public void CheckOZU()
+        {
+            double wait = 11.33;
+            double res = ds.SredOZU(LoadFromFileData(filePath));
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void CheckMAX()
+        {
+            double wait = 11;
+            double res = ds.MaxYadra(LoadFromFileData(filePath));
+            Assert.AreEqual(wait, res);
+        }
     }
 }
